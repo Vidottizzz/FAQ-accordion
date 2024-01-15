3d972289b5a68a2e7af5ff6f84a9614768a9931b
Vidotti <vidottizzz@gmail.com>
2024-01-15 15:09:15 -0300

    javascript toggle part done

---
+++ script/script.js
@@ -0,0 +1,22 @@
+
+const minusimg = document.getElementById('minus-img');
+
+
+function showhide() {
+    const hiddentext1 = document.querySelector("#hidden-text1");
+    hiddentext1.classList.toggle('hidden');
+}
+function showhide2() {
+      const hiddentext2 = document.querySelector("#hidden-text2");
+      hiddentext2.classList.toggle('hidden');
+  }
+
+function showhide3() {
+      const hiddentext3 = document.querySelector("#hidden-text3");
+      hiddentext3.classList.toggle('hidden');
+  }
+
+function showhide4() {
+      const hiddentext4 = document.querySelector("#hidden-text4");
+      hiddentext4.classList.toggle('hidden');
+  }
